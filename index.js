c53d0cd93866fb17c4bd3c3b65e887fa718dc625
Alex Volkov <vol4ikman@gmail.com>
2016-03-15 22:31:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+jQuery(document).ready(function(){
+  jQuery(".title_element").click(function(e){
+    e.preventDefault();
+    jQuery(this).parents('.acc_item').find('.content_element').slideToggle(200);
+  })
+});
